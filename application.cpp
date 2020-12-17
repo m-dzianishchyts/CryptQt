30411d41eab56e624484ec7aad428f0f254b2f1f
Mikhail Denishchits <denishicmihail@gmail.com>
2020-12-17 19:14:45 +0300

    Smart moving.
    * Mode name fix.
    * Clear file list when change mode or algorithm.
    * Clear RC4 key when change to RC4 algorithm.
    * Stage order fix.

--- application.cpp
+++ application.cpp
@@ -1,9 +1,12 @@
 #include "application.h"
 #include "ui_application.h"
 
+#include <QMessageBox>
+
 Application::Application(QWidget *parent) : QMainWindow(parent), ui(new Ui::Application) {
     ui->setupUi(this);
     setMaximumSize(800, 600);
+    setWindowIcon(QIcon("images/icon.ico"));
 
     for(auto groupBox : {ui->modeGroupBox, ui->algorithmGroupBox, ui->filesGroupBox,
                          ui->keyRC4GroupBox, ui->keyGroupBox, ui->processingGroupBox,
@@ -11,14 +14,17 @@ Application::Application(QWidget *parent) : QMainWindow(parent), ui(new Ui::Appl
         groupBox->move(260, 0);
         groupBox->hide();
     }
-
     stages.append(ui->welcomeGroupBox);
     stages.append(ui->modeGroupBox);
     stages.append(ui->algorithmGroupBox);
+    stages.append(ui->filesGroupBox);
     stages.append(ui->keyRC4GroupBox);
     stages.append(ui->processingGroupBox);
     stages.append(ui->completedGroupBox);
     currentGroupBox = new QList<QGroupBox*>::iterator(stages.begin());
+
+    encryptionAlgorithm = new QString(ui->algorithmComboBox->currentText());
+    mode = new QString(ui->modeComboBox->currentText());
 }
 
 Application::~Application() {
@@ -38,10 +44,55 @@ void Application::on_backButton_clicked() {
 void Application::on_nextButton_clicked() {
     currentGroupBox->i->t()->hide();
     (*currentGroupBox)++;
-    if (*currentGroupBox == stages.end() - 2) {
-        ui->nextButton->hide();
-        ui->backButton->hide();
-    }
+//    if (currentGroupBox->i->t() == ui->processingGroupBox) {
+//        ui->nextButton->hide();
+//        ui->backButton->hide();
+//    }
     currentGroupBox->i->t()->show();
     ui->backButton->setEnabled(true);
+}
+
+void Application::on_cancelButton_clicked() {
+    auto exitReply = QMessageBox::question(this, "Exit", "Are you sure you want to exit the CryptQt?",
+                                           QMessageBox::Yes | QMessageBox::No);
+    if (exitReply == QMessageBox::Yes) {
+        QApplication::quit();
+    }
+}
+
+void Application::on_modeComboBox_currentTextChanged(const QString &value) {
+    if (mode->compare(value) != 0) {
+        ui->fileList->clear();
+        *mode = value;
+        if (mode->compare("Encrypt") == 0) {
+            if (encryptionAlgorithm->compare("RC4") == 0) {
+                ui->keyRC4LineEdit->clear();
+                stages.replace(4, ui->keyRC4GroupBox);
+            } else {
+                stages.removeAt(4);
+            }
+        } else {
+            if (encryptionAlgorithm->compare("RC4") == 0) {
+                stages.replace(4, ui->keyGroupBox);
+            } else {
+                stages.insert(4, ui->keyGroupBox);
+            }
+        }
+    }
+}
+
+void Application::on_algorithmComboBox_currentTextChanged(const QString &value) {
+    QString oldAlgorithm = *encryptionAlgorithm;
+    if (oldAlgorithm.compare(value) != 0) {
+        ui->fileList->clear();
+        *encryptionAlgorithm = value;
+        if (mode->compare("Encrypt") == 0) {
+            if (encryptionAlgorithm->compare("RC4") == 0) {
+                ui->keyRC4LineEdit->clear();
+                stages.insert(4, ui->keyRC4GroupBox);
+            } else if (oldAlgorithm.compare("RC4") == 0) {
+                stages.removeAt(4);
+            }
+        }
+    }
 }
--- application.h
+++ application.h
@@ -19,12 +19,18 @@ private slots:
 
     void on_nextButton_clicked();
 
+    void on_cancelButton_clicked();
+
+    void on_modeComboBox_currentTextChanged(const QString &arg1);
+
+    void on_algorithmComboBox_currentTextChanged(const QString &arg1);
+
 private:
     Ui::Application *ui;
 
     QList<QGroupBox*>::iterator *currentGroupBox;
-    QString mode;
-    QString encryptionAlgorithm;
+    QString *mode;
+    QString *encryptionAlgorithm;
 
     QList<QGroupBox*> stages;
 };
