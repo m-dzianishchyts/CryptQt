583d11990b80b4dadf3a0d0b63f4d1d484981944
Mikhail Denishchits <denishicmihail@gmail.com>
2020-12-17 16:12:21 +0300

    Moving between stages.

--- application.cpp
+++ application.cpp
@@ -1,15 +1,47 @@
 #include "application.h"
 #include "ui_application.h"
 
-Application::Application(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Application)
-{
+Application::Application(QWidget *parent) : QMainWindow(parent), ui(new Ui::Application) {
     ui->setupUi(this);
+    setMaximumSize(800, 600);
+
+    for(auto groupBox : {ui->modeGroupBox, ui->algorithmGroupBox, ui->filesGroupBox,
+                         ui->keyRC4GroupBox, ui->keyGroupBox, ui->processingGroupBox,
+                         ui->completedGroupBox}) {
+        groupBox->move(260, 0);
+        groupBox->hide();
+    }
+
+    stages.append(ui->welcomeGroupBox);
+    stages.append(ui->modeGroupBox);
+    stages.append(ui->algorithmGroupBox);
+    stages.append(ui->keyRC4GroupBox);
+    stages.append(ui->processingGroupBox);
+    stages.append(ui->completedGroupBox);
+    currentGroupBox = new QList<QGroupBox*>::iterator(stages.begin());
 }
 
-Application::~Application()
-{
+Application::~Application() {
     delete ui;
 }
 
+
+void Application::on_backButton_clicked() {
+    currentGroupBox->i->t()->hide();
+    (*currentGroupBox)--;
+    if (*currentGroupBox == stages.begin()) {
+        ui->backButton->setEnabled(false);
+    }
+    currentGroupBox->i->t()->show();
+}
+
+void Application::on_nextButton_clicked() {
+    currentGroupBox->i->t()->hide();
+    (*currentGroupBox)++;
+    if (*currentGroupBox == stages.end() - 2) {
+        ui->nextButton->hide();
+        ui->backButton->hide();
+    }
+    currentGroupBox->i->t()->show();
+    ui->backButton->setEnabled(true);
+}
--- application.h
+++ application.h
@@ -1,19 +1,30 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QGroupBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Application; }
 QT_END_NAMESPACE
 
-class Application : public QMainWindow
-{
+class Application : public QMainWindow {
     Q_OBJECT
 
 public:
     Application(QWidget *parent = nullptr);
     ~Application();
 
+private slots:
+    void on_backButton_clicked();
+
+    void on_nextButton_clicked();
+
 private:
     Ui::Application *ui;
+
+    QList<QGroupBox*>::iterator *currentGroupBox;
+    QString mode;
+    QString encryptionAlgorithm;
+
+    QList<QGroupBox*> stages;
 };
